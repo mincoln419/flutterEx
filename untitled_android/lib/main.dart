b597e5e2708b913681a6c005c0095aec6ec8d9d1
mermer <mincoln419@naver.com>
2022-07-01 08:57:48 +0900

    statefull widget
    statefull widget

--- untitled_android/lib/main.dart
+++ untitled_android/lib/main.dart
@@ -10,24 +10,41 @@ class MyApp extends StatelessWidget {
     return MaterialApp(//material design 차용한 객체
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('헬로 월드'),
-          ),
-          body: Text('헬로 월드', style: TextStyle(fontSize: 30),
-          )),
+      home: HelloPage("Hello World")
     );
   }
 }
+class HelloPage extends StatefulWidget {
+  final String title;
+  HelloPage(this.title): super();
+
+  @override
+  State<HelloPage> createState() => _HelloPageState();
+}
+
+class _HelloPageState extends State<HelloPage> {
+  String _message = 'Hello World Flutter';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: _changeMessage),
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Text(_message, style: TextStyle(fontSize: 30)));
+  }
+  void _changeMessage() {
+    setState((){
+      _message = '헬로 월드';
+    });
+  }
+}
+
+
+
 
